71af33aa394a22580fb26abe4d6c7cbe4ec5d484
Joseph Lunine <jel99@drexel.edu>
2018-02-25 15:03:31 -0500

    added Phrases for each dex reading
    phrases!

--- main.js
+++ main.js
@@ -47,6 +47,9 @@
 
 // Determine the month entered by the user, find corresponding Pokémon to display.
 
+var pokemon;
+var name;
+
 function getPokemon() {
 
   var bday = document.getElementById('bday').value;
@@ -55,8 +58,6 @@ function getPokemon() {
 
   var whichMonth = date[1];
   var whichDayOfMonth = date[2];
-
-  var pokemon;
 
   if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
     pokemon = 'machoke';
@@ -90,18 +91,35 @@ function getPokemon() {
   return false;
 }
 
-
-var name;
-
 // Random phrase generator that outputs to the Dex Reading if the person entered a name.
 function getPhrase() {
-  
   name = getName();
+
+  var pokemonFL = pokemon.charAt(0).toUpperCase();
+  var pokemonT = pokemonFL + pokemon.slice(1);
   
   var phrases = [
-    "Ah, " + name + ", looks like you are a " + Charmeleon + ".",
-    ,
+    "Ah, " + name + ", looks like you are a " + pokemonT,
+    "Hmm, seems like you are " + pokemonT + ", " + name,
+    "How interesting! " + name + " , it turns out you're a " + pokemonT,
+    "Not bad, " + name + "! You are a " + pokemonT,
+    "I'm not sure how you'll take this, " + name + ", but you're a " + pokemonT,
+    "Now " + name + ", you'd better sit down for this: you're a " + pokemonT,
+    "Well well well, if it isn't " + name + " masquerading as a " + pokemonT,
+    "I guess " + pokemonT + " is your match " + name,
+    name + ", I'm shocked! You are, in fact, a " + pokemonT,
+    "Guess what, " + name + "? You're a " + pokemonT,
+    "Yer a " + pokemonT + " Harry!... I mean" + name,
+    "I wish I could be a " + pokemonT + " just like you, " + name
   ]
+
+  var randBetween = Math.floor(Math.random()*13);
+  console.log(randBetween);
+
+  var chosenPhrase = phrases[randBetween];
+  console.log(chosenPhrase);
+
+  return chosenPhrase;
 }
 
 // Display the appropriate Pokédex entry for the user
@@ -113,10 +131,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         chaRead.textContent = "Like a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
       } else {
-        chaRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        chaRead.textContent = chosenPhrase + ". Like a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
       }
 
       showZodiac(pokemon);
@@ -129,10 +149,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         digRead.textContent = "As a Diglett, you are reliable, practical, and responsible. You won't let another Diglett down, or let those pesky humans build a dam in your forest. However, being possesive is one of your weaknesses that you need to work on.";
       } else {
-        digRead.textContent = "Hmm seems like you are a Diglett, " + name +". As a Diglett, you are reliable, practical, and responsible. You won't let another Diglett down, or let those pesky humans build a dam in your forest. However, being possesive is one of your weaknesses that you need to work on.";
+        digRead.textContent = chosenPhrase + ". As a Diglett, you are reliable, practical, and responsible. You won't let another Diglett down, or let those pesky humans build a dam in your forest. However, being possesive is one of your weaknesses that you need to work on.";
       }
 
       showZodiac(pokemon);
@@ -144,10 +166,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         pidRead.textContent = "Like a Pidgey, you are a gentle, affectionate person who doesn't like to get into fights if you don't have to. However, if danger approaches, you will kick up sand into your opponent's face to get away. Being nervous like this is one of your weaknesses.";
       } else {
-        pidRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        pidRead.textContent = chosenPhrase + ". Like a Pidgey, you are a gentle, affectionate person who doesn't like to get into fights if you don't have to. However, if danger approaches, you will kick up sand into your opponent's face to get away. Being nervous like this is one of your weaknesses.";
       }
 
       showZodiac(pokemon);
@@ -159,10 +183,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         lapRead.textContent = "As a Lapras, you are a very loyal, emotional, and sympathetic person. You will ferry your friends to safety if they need it. However, you can be moody (possibly since you have been hunted to near extinction), and also insecure.";
       } else {
-        lapRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        lapRead.textContent = chosenPhrase + ". As a Lapras, you are a very loyal, emotional, and sympathetic person. You will ferry your friends to safety if they need it. However, you can be moody (possibly since you have been hunted to near extinction), and also insecure.";
       }
 
       showZodiac(pokemon);
@@ -174,10 +200,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         arcRead.textContent = "Like an Arcanine, you enjoy being admired for your beauty and magnificence. However, sometimes this admiration can make you a bit arrogant and self-centered. But you also are very warm-hearted, generous, and cheerful, always looking on the bright side of life. You can cheer your friends up easily when they're down.";
       } else {
-        arcRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        arcRead.textContent = chosenPhrase + ". Like an Arcanine, you enjoy being admired for your beauty and magnificence. However, sometimes this admiration can make you a bit arrogant and self-centered. But you also are very warm-hearted, generous, and cheerful, always looking on the bright side of life. You can cheer your friends up easily when they're down.";
       }
 
       showZodiac(pokemon);
@@ -189,10 +217,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         sanRead.textContent = "As a Sandshrew, you can adapt easily to whatever is thrown at you in life. You are very analytical in how you approach tasks, but can also be overly critical of yourself, possibly working too much and pushing people away because of it.";
       } else {
-        sanRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        sanRead.textContent = chosenPhrase + ". As a Sandshrew, you can adapt easily to whatever is thrown at you in life. You are very analytical in how you approach tasks, but can also be overly critical of yourself, possibly working too much and pushing people away because of it.";
       }
 
       showZodiac(pokemon);
@@ -204,10 +234,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         dodRead.textContent = "Like a Dodrio, you are very good at cooperating within your own mind, and can execute detailed plans easily. Because of this, you are very diplomatic as well. At some times, however, you can be indecisive as to what to do in some situations.";
       } else {
-        dodRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        dodRead.textContent = chosenPhrase + ". Like a Dodrio, you are very good at cooperating within your own mind, and can execute detailed plans easily. Because of this, you are very diplomatic as well. At some times, however, you can be indecisive as to what to do in some situations.";
       }
 
       showZodiac(pokemon);
@@ -219,10 +251,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         staRead.textContent = "As a Staryu, you are very resourceful and brave in a battle, being able to regenerate any part of yourself at will. However, not wanting anybody to figure out your one weak spot, you can be distrusting and secretive.";
       } else {
-        staRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        staRead.textContent = chosenPhrase + ". As a Staryu, you are very resourceful and brave in a battle, being able to regenerate any part of yourself at will. However, not wanting anybody to figure out your one weak spot, you can be distrusting and secretive.";
       }
 
       showZodiac(pokemon);
@@ -234,10 +268,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         rapRead.textContent = "Like a Rapidash, you enjoy freedom, traveling fast, and being in the great outdoors. You really just love to run, and will chase after anything at top speed if you see that it's faster than you. However, because of being so speedy all the time, you can be really impatient when things get slow for you.";
       } else {
-        rapRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        rapRead.textContent = chosenPhrase + ". Like a Rapidash, you enjoy freedom, traveling fast, and being in the great outdoors. You really just love to run, and will chase after anything at top speed if you see that it's faster than you. However, because of being so speedy all the time, you can be really impatient when things get slow for you.";
       }
 
       showZodiac(pokemon);
@@ -249,10 +285,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         macRead.textContent = "As a Machoke, you are responsible and disciplined, always going to the gym on time. You also enjoy helping friends move and will carry all their stuff for them. However, due to your disciplined nature, you can be somewhat condescending at times to others.";
       } else {
-        macRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        macRead.textContent = chosenPhrase + ". As a Machoke, you are responsible and disciplined, always going to the gym on time. You also enjoy helping friends move and will carry all their stuff for them. However, due to your disciplined nature, you can be somewhat condescending at times to others.";
       }
 
       showZodiac(pokemon);
@@ -264,10 +302,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         draRead.textContent = "Like a Dragonite, you are very progressive in your ideologies, and are extremely independent. You do like to spend time with friends a lot, but can be uncompromising and temperamental at times.";
       } else {
-        draRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        draRead.textContent = chosenPhrase + ". Like a Dragonite, you are very progressive in your ideologies, and are extremely independent. You do like to spend time with friends a lot, but can be uncompromising and temperamental at times.";
       }
 
       showZodiac(pokemon);
@@ -279,10 +319,12 @@ function displayPokedex(pokemon) {
       clearZodiac();
 
       name = getName();
+      chosenPhrase = getPhrase();
+
       if (name == "") {
         golRead.textContent = "As a Goldeen, you really enjoy a good swim in your local river. To your friends, you are known as the Water Queen/King. You are also a very compassionate, gentle, and artistic person. However, you can sometimes be overly trusting of people, as well as too fearful of what lies ahead in the great river of life.";
       } else {
-        golRead.textContent = "Ah, " + name + ", looks like you are a Charmeleon. Similar to a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        golRead.textContent = chosenPhrase + ". As a Goldeen, you really enjoy a good swim in your local river. To your friends, you are known as the Water Queen/King. You are also a very compassionate, gentle, and artistic person. However, you can sometimes be overly trusting of people, as well as too fearful of what lies ahead in the great river of life.";
       }
 
       showZodiac(pokemon);
@@ -312,9 +354,13 @@ function showForm() {
 }
 
 function getName() {
-  var fname = document.getElementById('fName').value;
+  var fnameT = document.getElementById('fName').value;
       if (fname == "") { console.log('No name retrieved.'); }
       else { console.log('Name was retrieved.'); }
+
+  var fletter = fnameT.charAt(0).toUpperCase();
+  var fname = fletter + fnameT.slice(1);
+  
   return fname;
 }
 
