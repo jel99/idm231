adb3e06ea84876429e93f670769ce52775f8e194
Joseph Lunine <jel99@drexel.edu>
2018-02-25 18:13:58 -0500

    updates
    updates

--- main.js
+++ main.js
@@ -28,9 +28,7 @@
   dragonite.addEventListener('click', function() {displayPokedex('dragonite');});
   goldeen.addEventListener('click', function() {displayPokedex('goldeen');});
 
-
-  
-
+  // Get areas where zodiac phrase will output
   var chaRead = document.getElementById('chaRead');
   var digRead = document.getElementById('digRead');
   var pidRead = document.getElementById('pidRead');
@@ -46,18 +44,27 @@
 
 
 // Determine the month entered by the user, find corresponding Pokémon to display.
-
 var pokemon;
 var name;
+var phrase;
 
 function getPokemon() {
 
+  //var bday = document.getElementById('bday').value;
+
+
+  // STILL NEED TO GET THIS TO CORRECT IF THEY PUT IN BAD VALUE!
   var bday = document.getElementById('bday').value;
+  var date = bday.split('/');
 
-  var date = bday.split('-');
+  
+  //var date = bday.split('-');
 
-  var whichMonth = date[1];
-  var whichDayOfMonth = date[2];
+  // var whichMonth = date[1];
+  // var whichDayOfMonth = date[2];
+
+  var whichMonth = date[0];
+  var whichDayOfMonth = date[1];
 
   if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
     pokemon = 'machoke';
@@ -92,28 +99,47 @@ function getPokemon() {
 }
 
 // Random phrase generator that outputs to the Dex Reading if the person entered a name.
-function getPhrase() {
+function getPhrase(pokemonTemp) {
   name = getName();
 
-  var pokemonFL = pokemon.charAt(0).toUpperCase();
-  var pokemonT = pokemonFL + pokemon.slice(1);
+  var pokemonFL = pokemonTemp.charAt(0).toUpperCase();
+  var pokemonT = pokemonFL + pokemonTemp.slice(1);
   
-  var phrases = [
-    "Ah, " + name + ", looks like you are a " + pokemonT,
-    "Hmm, seems like you are " + pokemonT + ", " + name,
-    "How interesting! " + name + " , it turns out you're a " + pokemonT,
-    "Not bad, " + name + "! You are a " + pokemonT,
-    "I'm not sure how you'll take this, " + name + ", but you're a " + pokemonT,
-    "Now " + name + ", you'd better sit down for this: you're a " + pokemonT,
-    "Well well well, if it isn't " + name + " masquerading as a " + pokemonT,
-    "I guess " + pokemonT + " is your match " + name,
-    name + ", I'm shocked! You are, in fact, a " + pokemonT,
-    "Guess what, " + name + "? You're a " + pokemonT,
-    "Yer a " + pokemonT + " Harry!... I mean" + name,
-    "I wish I could be a " + pokemonT + " just like you, " + name
-  ]
+  var phrases;
+  if (pokemonT == "Arcanine"){
+    phrases = [
+      /* 0  */"Ah, " + name + ", looks like you are an " + pokemonT,
+      /* 1  */"Hmm, seems like you are an " + pokemonT + ", " + name,
+      /* 2  */"How interesting! " + name + ", it turns out you're an " + pokemonT,
+      /* 3  */"Not bad, " + name + "! You are an " + pokemonT,
+      /* 4  */"I'm not sure how you'll take this, " + name + ", but you're an " + pokemonT,
+      /* 5  */"Now " + name + ", you'd better sit down for this: you're an " + pokemonT,
+      /* 6  */"Well well well, if it isn't " + name + " masquerading as an " + pokemonT,
+      /* 7  */"The results are in " + name + ": You are most similar to an " + pokemonT,
+      /* 8  */name + ", I'm shocked! You are, in fact, an " + pokemonT,
+      /* 9  */"Guess what, " + name + "? You're an " + pokemonT,
+      /* 10 */"Yer an " + pokemonT + " Harry!... I mean " + name,
+      /* 11 */"I wish I could be an " + pokemonT + " just like you, " + name
+    ];
+  }
+  else {
+    phrases = [
+      /* 0  */"Ah, " + name + ", looks like you are a " + pokemonT,
+      /* 1  */"Hmm, seems like you are a " + pokemonT + ", " + name,
+      /* 2  */"How interesting! " + name + ", it turns out you're a " + pokemonT,
+      /* 3  */"Not bad, " + name + "! You are a " + pokemonT,
+      /* 4  */"I'm not sure how you'll take this, " + name + ", but you're a " + pokemonT,
+      /* 5  */"Now " + name + ", you'd better sit down for this: you're a " + pokemonT,
+      /* 6  */"Well well well, if it isn't " + name + " masquerading as a " + pokemonT,
+      /* 7  */"The results are in " + name + ": You are most similar to a " + pokemonT,
+      /* 8  */name + ", I'm shocked! You are, in fact, a " + pokemonT,
+      /* 9  */"Guess what, " + name + "? You're a " + pokemonT,
+      /* 10 */"Yer a " + pokemonT + " Harry!... I mean " + name,
+      /* 11 */"I wish I could be a " + pokemonT + " just like you, " + name
+    ];
+  }
 
-  var randBetween = Math.floor(Math.random()*13);
+  var randBetween = Math.floor(Math.random()*12);
   console.log(randBetween);
 
   var chosenPhrase = phrases[randBetween];
@@ -122,21 +148,23 @@ function getPhrase() {
   return chosenPhrase;
 }
 
+
 // Display the appropriate Pokédex entry for the user
 function displayPokedex(pokemon) {
+
+  name = getName();
+  phrase = getPhrase(pokemon);
+
   switch (pokemon) {
 
     case 'charmeleon':
       console.log('Ran charmeleon');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         chaRead.textContent = "Like a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
       } else {
-        chaRead.textContent = chosenPhrase + ". Like a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
+        chaRead.textContent = phrase + ". Like a Charmeleon, you are a confident person who won't stop until you've gotten what you want. One weakness of yours is that you're hot-headed and will scratch anyone who makes you mad with your razor-sharp claws.";
       }
 
       showZodiac(pokemon);
@@ -148,13 +176,10 @@ function displayPokedex(pokemon) {
       console.log('Ran diglett');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         digRead.textContent = "As a Diglett, you are reliable, practical, and responsible. You won't let another Diglett down, or let those pesky humans build a dam in your forest. However, being possesive is one of your weaknesses that you need to work on.";
       } else {
-        digRead.textContent = chosenPhrase + ". As a Diglett, you are reliable, practical, and responsible. You won't let another Diglett down, or let those pesky humans build a dam in your forest. However, being possesive is one of your weaknesses that you need to work on.";
+        digRead.textContent = phrase + ". As a Diglett, you are reliable, practical, and responsible. You won't let another Diglett down, or let those pesky humans build a dam in your forest. However, being possesive is one of your weaknesses that you need to work on.";
       }
 
       showZodiac(pokemon);
@@ -165,13 +190,10 @@ function displayPokedex(pokemon) {
       console.log('Ran pidgey');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         pidRead.textContent = "Like a Pidgey, you are a gentle, affectionate person who doesn't like to get into fights if you don't have to. However, if danger approaches, you will kick up sand into your opponent's face to get away. Being nervous like this is one of your weaknesses.";
       } else {
-        pidRead.textContent = chosenPhrase + ". Like a Pidgey, you are a gentle, affectionate person who doesn't like to get into fights if you don't have to. However, if danger approaches, you will kick up sand into your opponent's face to get away. Being nervous like this is one of your weaknesses.";
+        pidRead.textContent = phrase + ". Like a Pidgey, you are a gentle, affectionate person who doesn't like to get into fights if you don't have to. However, if danger approaches, you will kick up sand into your opponent's face to get away. Being nervous like this is one of your weaknesses.";
       }
 
       showZodiac(pokemon);
@@ -182,13 +204,10 @@ function displayPokedex(pokemon) {
       console.log('Ran lapras');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         lapRead.textContent = "As a Lapras, you are a very loyal, emotional, and sympathetic person. You will ferry your friends to safety if they need it. However, you can be moody (possibly since you have been hunted to near extinction), and also insecure.";
       } else {
-        lapRead.textContent = chosenPhrase + ". As a Lapras, you are a very loyal, emotional, and sympathetic person. You will ferry your friends to safety if they need it. However, you can be moody (possibly since you have been hunted to near extinction), and also insecure.";
+        lapRead.textContent = phrase + ". As a Lapras, you are a very loyal, emotional, and sympathetic person. You will ferry your friends to safety if they need it. However, you can be moody (possibly since you have been hunted to near extinction), and also insecure.";
       }
 
       showZodiac(pokemon);
@@ -199,13 +218,10 @@ function displayPokedex(pokemon) {
       console.log('Ran arcanine');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         arcRead.textContent = "Like an Arcanine, you enjoy being admired for your beauty and magnificence. However, sometimes this admiration can make you a bit arrogant and self-centered. But you also are very warm-hearted, generous, and cheerful, always looking on the bright side of life. You can cheer your friends up easily when they're down.";
       } else {
-        arcRead.textContent = chosenPhrase + ". Like an Arcanine, you enjoy being admired for your beauty and magnificence. However, sometimes this admiration can make you a bit arrogant and self-centered. But you also are very warm-hearted, generous, and cheerful, always looking on the bright side of life. You can cheer your friends up easily when they're down.";
+        arcRead.textContent = phrase + ". Like an Arcanine, you enjoy being admired for your beauty and magnificence. However, sometimes this admiration can make you a bit arrogant and self-centered. But you also are very warm-hearted, generous, and cheerful, always looking on the bright side of life. You can cheer your friends up easily when they're down.";
       }
 
       showZodiac(pokemon);
@@ -216,13 +232,10 @@ function displayPokedex(pokemon) {
       console.log('Ran sandshrew');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         sanRead.textContent = "As a Sandshrew, you can adapt easily to whatever is thrown at you in life. You are very analytical in how you approach tasks, but can also be overly critical of yourself, possibly working too much and pushing people away because of it.";
       } else {
-        sanRead.textContent = chosenPhrase + ". As a Sandshrew, you can adapt easily to whatever is thrown at you in life. You are very analytical in how you approach tasks, but can also be overly critical of yourself, possibly working too much and pushing people away because of it.";
+        sanRead.textContent = phrase + ". As a Sandshrew, you can adapt easily to whatever is thrown at you in life. You are very analytical in how you approach tasks, but can also be overly critical of yourself, possibly working too much and pushing people away because of it.";
       }
 
       showZodiac(pokemon);
@@ -233,13 +246,10 @@ function displayPokedex(pokemon) {
       console.log('Ran dodrio');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         dodRead.textContent = "Like a Dodrio, you are very good at cooperating within your own mind, and can execute detailed plans easily. Because of this, you are very diplomatic as well. At some times, however, you can be indecisive as to what to do in some situations.";
       } else {
-        dodRead.textContent = chosenPhrase + ". Like a Dodrio, you are very good at cooperating within your own mind, and can execute detailed plans easily. Because of this, you are very diplomatic as well. At some times, however, you can be indecisive as to what to do in some situations.";
+        dodRead.textContent = phrase + ". Like a Dodrio, you are very good at cooperating within your own mind, and can execute detailed plans easily. Because of this, you are very diplomatic as well. At some times, however, you can be indecisive as to what to do in some situations.";
       }
 
       showZodiac(pokemon);
@@ -250,13 +260,10 @@ function displayPokedex(pokemon) {
       console.log('Ran staryu');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         staRead.textContent = "As a Staryu, you are very resourceful and brave in a battle, being able to regenerate any part of yourself at will. However, not wanting anybody to figure out your one weak spot, you can be distrusting and secretive.";
       } else {
-        staRead.textContent = chosenPhrase + ". As a Staryu, you are very resourceful and brave in a battle, being able to regenerate any part of yourself at will. However, not wanting anybody to figure out your one weak spot, you can be distrusting and secretive.";
+        staRead.textContent = phrase + ". As a Staryu, you are very resourceful and brave in a battle, being able to regenerate any part of yourself at will. However, not wanting anybody to figure out your one weak spot, you can be distrusting and secretive.";
       }
 
       showZodiac(pokemon);
@@ -267,13 +274,10 @@ function displayPokedex(pokemon) {
       console.log('Ran rapidash');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         rapRead.textContent = "Like a Rapidash, you enjoy freedom, traveling fast, and being in the great outdoors. You really just love to run, and will chase after anything at top speed if you see that it's faster than you. However, because of being so speedy all the time, you can be really impatient when things get slow for you.";
       } else {
-        rapRead.textContent = chosenPhrase + ". Like a Rapidash, you enjoy freedom, traveling fast, and being in the great outdoors. You really just love to run, and will chase after anything at top speed if you see that it's faster than you. However, because of being so speedy all the time, you can be really impatient when things get slow for you.";
+        rapRead.textContent = phrase + ". Like a Rapidash, you enjoy freedom, traveling fast, and being in the great outdoors. You really just love to run, and will chase after anything at top speed if you see that it's faster than you. However, because of being so speedy all the time, you can be really impatient when things get slow for you.";
       }
 
       showZodiac(pokemon);
@@ -284,13 +288,10 @@ function displayPokedex(pokemon) {
       console.log('Ran machoke');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         macRead.textContent = "As a Machoke, you are responsible and disciplined, always going to the gym on time. You also enjoy helping friends move and will carry all their stuff for them. However, due to your disciplined nature, you can be somewhat condescending at times to others.";
       } else {
-        macRead.textContent = chosenPhrase + ". As a Machoke, you are responsible and disciplined, always going to the gym on time. You also enjoy helping friends move and will carry all their stuff for them. However, due to your disciplined nature, you can be somewhat condescending at times to others.";
+        macRead.textContent = phrase + ". As a Machoke, you are responsible and disciplined, always going to the gym on time. You also enjoy helping friends move and will carry all their stuff for them. However, due to your disciplined nature, you can be somewhat condescending at times to others.";
       }
 
       showZodiac(pokemon);
@@ -301,13 +302,10 @@ function displayPokedex(pokemon) {
       console.log('Ran dragonite');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         draRead.textContent = "Like a Dragonite, you are very progressive in your ideologies, and are extremely independent. You do like to spend time with friends a lot, but can be uncompromising and temperamental at times.";
       } else {
-        draRead.textContent = chosenPhrase + ". Like a Dragonite, you are very progressive in your ideologies, and are extremely independent. You do like to spend time with friends a lot, but can be uncompromising and temperamental at times.";
+        draRead.textContent = phrase + ". Like a Dragonite, you are very progressive in your ideologies, and are extremely independent. You do like to spend time with friends a lot, but can be uncompromising and temperamental at times.";
       }
 
       showZodiac(pokemon);
@@ -318,13 +316,10 @@ function displayPokedex(pokemon) {
       console.log('Ran goldeen');
       clearZodiac();
 
-      name = getName();
-      chosenPhrase = getPhrase();
-
       if (name == "") {
         golRead.textContent = "As a Goldeen, you really enjoy a good swim in your local river. To your friends, you are known as the Water Queen/King. You are also a very compassionate, gentle, and artistic person. However, you can sometimes be overly trusting of people, as well as too fearful of what lies ahead in the great river of life.";
       } else {
-        golRead.textContent = chosenPhrase + ". As a Goldeen, you really enjoy a good swim in your local river. To your friends, you are known as the Water Queen/King. You are also a very compassionate, gentle, and artistic person. However, you can sometimes be overly trusting of people, as well as too fearful of what lies ahead in the great river of life.";
+        golRead.textContent = phrase + ". As a Goldeen, you really enjoy a good swim in your local river. To your friends, you are known as the Water Queen/King. You are also a very compassionate, gentle, and artistic person. However, you can sometimes be overly trusting of people, as well as too fearful of what lies ahead in the great river of life.";
       }
 
       showZodiac(pokemon);
@@ -334,12 +329,14 @@ function displayPokedex(pokemon) {
 }
 
 
+// Show the zodiac of the person's Pokématch
 function showZodiac(pokemon) {
   document.getElementById(pokemon).setAttribute('class', 'dexEntry showZodiac');
   document.getElementById('inputForm').setAttribute('class', 'inputForm showZodiac');
 }
 
 
+// Remove the zodiac from the screen
 function clearZodiac() {
   var dexEntries = document.getElementsByClassName('dexEntry');
 
@@ -349,10 +346,13 @@ function clearZodiac() {
 }
 
 
+// Show the name/date entry form again
 function showForm() {
   document.getElementById('inputForm').setAttribute('class', 'inputForm');
 }
 
+
+// Get the person's first name for the phrase
 function getName() {
   var fnameT = document.getElementById('fName').value;
       if (fname == "") { console.log('No name retrieved.'); }
@@ -364,6 +364,8 @@ function getName() {
   return fname;
 }
 
+
+// Play the Pokémon's cry when the zodiac is opened
 function playSound(pokemon) {
   var music = document.getElementById(pokemon);
 
